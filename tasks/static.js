7ace4f929924a98dbf73de87b0d5c26478d2eac5
Jon Uhlmann <me@jonnitto.ch>
2017-12-19 23:08:36 +0100

    :building_construction: Refactor static task

--- tasks/static.js
+++ tasks/static.js
@@ -4,35 +4,51 @@ if (!config.tasks.static) {
     return false;
 }
 
-let paths = {
-    src: path.join(
-        config.root.base,
-        config.root.src,
-        config.tasks.static.src,
-        "/**",
-        getExtensions(config.tasks.static.extensions)
-    ),
-    dest: path.join(
-        config.root.base,
-        config.root.dest,
-        config.tasks.static.dest
-    )
-};
+const PACKAGES_CONFIG = [];
+for (let key in config.packages) {
+    const CONFIG = config.packages[key];
+    const STATIC_CONFIG = CONFIG.tasks.static;
+
+    if (STATIC_CONFIG) {
+        PACKAGES_CONFIG.push({
+            key: key,
+            src: path.join(
+                CONFIG.root.base,
+                key,
+                CONFIG.root.src,
+                STATIC_CONFIG.src,
+                "/**",
+                getExtensions(STATIC_CONFIG.extensions)
+            ),
+            dest: path.join(
+                CONFIG.root.base,
+                key,
+                CONFIG.root.dest,
+                STATIC_CONFIG.dest
+            )
+        });
+    }
+}
 
 function copy() {
-    return gulp
-        .src(paths.src, { since: cache.lastMtime("static") })
-        .pipe(plumber(handleErrors))
-        .pipe(cache("static"))
-        .pipe(changed(paths.dest)) // Ignore unchanged files
-        .pipe(chmod(config.chmod))
-        .pipe(gulp.dest(paths.dest))
-        .pipe(
-            size({
-                title: "Static Files:",
-                showFiles: false
+    let tasks = PACKAGES_CONFIG.map(packageConfig => {
+        return gulp
+            .src(packageConfig.src, {
+                since: cache.lastMtime(`${packageConfig.key}.static`)
             })
-        );
+            .pipe(plumber(handleErrors))
+            .pipe(cache(`${packageConfig.key}.static`))
+            .pipe(changed(packageConfig.dest)) // Ignore unchanged files
+            .pipe(chmod(config.global.chmod))
+            .pipe(gulp.dest(packageConfig.dest))
+            .pipe(
+                size({
+                    title: `${packageConfig.key} Static Files:`,
+                    showFiles: false
+                })
+            );
+    });
+    return merge(tasks);
 }
 
 module.exports = copy;
