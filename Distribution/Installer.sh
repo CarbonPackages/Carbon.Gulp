b302ba7122c436f9149d577e2d44fa6f6edf374d
Jon Uhlmann <me@jonnitto.ch>
2018-01-12 16:48:47 +0100

    :art: Improve check for Neos version

--- Distribution/Installer.sh
+++ Distribution/Installer.sh
@@ -24,19 +24,25 @@ if which json > /dev/null
     echo "${distribution},{\"browserslist\":$browserslist},{\"dependencies\":$dependencies}" | json --merge > package.json
 
     # composer.json
-    echo "- composer.json"
     composer=$(cat composer.json)
-
     replacedComposer=${composer//\\/\\\\}
     composer show neos/neos -q -n > /dev/null 2>&1
     if [ $? -eq 0 ]
       then
         version="Neos"
-      else
+    fi
+    composer show typo3/neos -q -n > /dev/null 2>&1
+    if [ $? -eq 0 ]
+      then
         version="TYPO3"
     fi
-    scripts=$(cat Build/Gulp/Distribution/${version}.json)
-    echo "${replacedComposer},${scripts}" | json --deep-merge -4 > composer.json
+    if [ $version ]
+      then
+        echo "- composer.json"
+        scripts=$(cat Build/Gulp/Distribution/${version}.json)
+        echo "${replacedComposer},${scripts}" | json --deep-merge -4 > composer.json
+    fi
+
   else
     echo "If you want to merge json files, you need to install following package globally:"
     echo "${RED}https://www.npmjs.com/package/json${NC}"
